d2de03cbb1286c404f5d4cb9ee503b03443f67a3
sho-t <aznecosann@gmail.com>
2018-07-23 12:13:05 +0900

    initial commit

---
+++ src/main/java/com/example/demoapi/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demoapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@GetMapping("/get")
+	public String get() {
+		return "hoge";
+	}
+}
